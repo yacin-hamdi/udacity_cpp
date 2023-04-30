4e18dc13f5ee941abae28aa19bc904d7b3b7246a
yacin hamdi <117452150+yacin-hamdi@users.noreply.github.com>
2023-04-30 09:32:27 +0100

    concurrency

---
+++ concurrency/running_single_thread/quiz.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+
+void threadFunctionEven() {
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));  // simulate work
+  std::cout << "Even thread\n";
+}
+
+void threadFunctionOdd(){
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  std::cout << "Odd thread\n";
+}
+
+int main() {
+  /* Student Task START */
+  for(int i=0;i<6;i++){
+    if(i%2 == 0){
+      std::thread t(threadFunctionEven);
+      t.detach();
+    }else{
+      std::thread t(threadFunctionOdd);
+      t.detach();
+    }
+  }
+
+  /* Student Task END */
+
+  // ensure that main does not return before the threads are finished
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));  // simulate work
+
+  std::cout << "End of main is reached" << std::endl;
+  return 0;
+}
