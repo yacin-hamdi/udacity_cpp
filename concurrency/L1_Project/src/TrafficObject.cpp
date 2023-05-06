822eac9eccaa859ce46307dc9583b1425c2e27e2
yacin hamdi <117452150+yacin-hamdi@users.noreply.github.com>
2023-05-06 16:59:08 +0100

    L1_proj

---
+++ concurrency/L1_Project/src/TrafficObject.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <chrono>
+#include "TrafficObject.h"
+#include<algorithm>
+
+// init static variable
+int TrafficObject::_idCnt = 0;
+
+void TrafficObject::setPosition(double x, double y)
+{
+    _posX = x;
+    _posY = y;
+}
+
+void TrafficObject::getPosition(double &x, double &y)
+{
+    x = _posX;
+    y = _posY;
+}
+
+TrafficObject::TrafficObject()
+{
+    _type = ObjectType::noObject;
+    _id = _idCnt++;
+}
+
+TrafficObject::~TrafficObject()
+{
+    // Task L1.1 : Set up a thread barrier that ensures that all the thread objects in the member vector _threads are joined.
+    std::for_each(_threads.begin(), _threads.end(), [](std::thread &t){
+        t.join();
+    });
+
+    
+}
